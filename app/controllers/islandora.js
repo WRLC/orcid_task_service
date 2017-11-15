7067eb1e5cc233d6f07c549f3ab7ecbe3b7f45c4
ihardy <ihardy@wrlc.org>
2017-11-15 15:34:27 -0500

    frame out create or update

--- app/controllers/islandora.js
+++ app/controllers/islandora.js
@@ -4,12 +4,10 @@ var mongoose = require('mongoose'),
     Researcher = mongoose.model('Researcher'),
     request = require('request');
 
-// controllers
+// utility
 
-exports.test_islandora = function(req, res) {
-	var cookieJar = request.jar();
+function islandora_auth() {
 	request.post({url: config.islandora.host + '/user/login',
-		jar: cookieJar,
 		form: {
 			'form_id': 'user_login',
 			'name': config.islandora.user,
@@ -21,6 +19,12 @@ exports.test_islandora = function(req, res) {
 		if(err)
 			console.log(err);
 	});
+}
+
+// controllers
+
+exports.test_islandora = function(req, res) {
+	islandora_auth();
 
 	request.get(config.islandora.host + config.islandora.testitem, function(island_err, island_res, island_body) {
 		if (island_err)
@@ -30,5 +34,22 @@ exports.test_islandora = function(req, res) {
 };
 
 exports.update_islandora = function(req, res) {
-	res.json({message: 'update function goes here'});
+	islandora_auth();
+    
+    request.get(config.islandora.host +
+    	'/islandora/rest/v1/solr/MADS_email_ms:' +
+    	req.body.identifier.netid,
+    	function(island_err, island_res, island_body) {
+    		if (island_err)
+    			return res.status(400).json({message: 'Researcher lookup failed.'});
+    		if (JSON.parse(island_body).response.numFound == 1) {
+    			res.json({message: 'update researcher in islandora'});
+    		} else if (JSON.parse(island_body).response.numFound == 0){
+    			res.json({message: 'create researcher in islandora'});
+    		} else {
+    			return res.status(500).json({message: 'Internal Error, could not parse islandora response.'})
+    		}
+
+    	});
+
 };
