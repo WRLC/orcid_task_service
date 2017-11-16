91a7dfc45c393bf48ee8323130fed3b65d9e3052
ihardy <ihardy@wrlc.org>
2017-11-16 13:16:36 -0500

    add create researcher function

--- app/controllers/islandora.js
+++ app/controllers/islandora.js
@@ -1,8 +1,8 @@
 // app/controllers/islandora.js
 var mongoose = require('mongoose'),
-    config = require('../../config'),
-    Researcher = mongoose.model('Researcher'),
-    request = require('request');
+	config = require('../../config'),
+	Researcher = mongoose.model('Researcher'),
+	request = require('request');
 
 // utility
 
@@ -26,7 +26,9 @@ function islandora_auth() {
 exports.test_islandora = function(req, res) {
 	islandora_auth();
 
-	request.get(config.islandora.host + config.islandora.testitem, function(island_err, island_res, island_body) {
+	request.get(config.islandora.host + 
+		config.islandora.testitem, 
+		function(island_err, island_res, island_body) {
 		if (island_err)
 			res.status(400).send(island_err);
 		res.send(island_body);
@@ -35,21 +37,42 @@ exports.test_islandora = function(req, res) {
 
 exports.update_islandora = function(req, res) {
 	islandora_auth();
-    
-    request.get(config.islandora.host +
-    	'/islandora/rest/v1/solr/MADS_email_ms:' +
-    	req.body.identifier.netid,
-    	function(island_err, island_res, island_body) {
-    		if (island_err)
-    			return res.status(400).json({message: 'Researcher lookup failed.'});
-    		if (JSON.parse(island_body).response.numFound == 1) {
-    			res.json({message: 'update researcher in islandora'});
-    		} else if (JSON.parse(island_body).response.numFound == 0){
-    			res.json({message: 'create researcher in islandora'});
-    		} else {
-    			return res.status(500).json({message: 'Internal Error, could not parse islandora response.'})
-    		}
+	
+	request.get(config.islandora.host +
+		'/islandora/rest/v1/solr/MADS_email_ms:' +
+		req.body.identifier.netid,
+		function(island_err, island_res, island_body) {
+			if (island_err)
+				return res.status(400).json({
+					message: 'Researcher lookup failed.'
+				});
+			if (JSON.parse(island_body).response.numFound == 1) {
+				res.json({
+					message: 'update researcher in islandora'
+				});
+			} else if (JSON.parse(island_body).response.numFound == 0) {
+				request.post({
+					url: config.islandora.host + '/islandora/rest/v1/object',
+					headers: {"content-type" :"application/json"},
+					body: {
+						"namespace" :"auislandora",
+						"label" : req.body.authority.name.given + ' ' +
+							req.body.authority.name.family,
+						"owner" : "admin"
+						},
+						json: true,
+					},
+				function(create_err, create_res, create_body) {
+					if(create_err)
+						 res.send(create_err);
+					res.send(create_body);
+				});
+			} else {
+				return res.status(500).json({
+					message: 'Internal Error, could not parse islandora response.'
+				})
+			}
 
-    	});
+		});
 
 };
